07a2a7734f221333cab0847311806f9e39d4e908
dok408 <dok408@vfi.local>
2016-05-24 14:46:59 +0200

    Projekt hinzugefügt

---
+++ Transport-Abfragen/Form1.cs
@@ -0,0 +1,30 @@
+﻿using SwissTransport;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Transport_Abfragen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonhello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sali");
+
+            Transport transport = new Transport();
+
+            MessageBox.Show(transport.ToString());
+        }
+    }
+}
