020845aad974390507c46756e768539e0dece9c5
dok408 <dok408@DOK408>
2016-05-30 14:37:17 +0200

    Weitere Buttons angebracht und ausprogrammiert

--- Transport-Abfragen/Form1.Designer.cs
+++ Transport-Abfragen/Form1.Designer.cs
@@ -34,19 +34,19 @@
             this.departure = new System.Windows.Forms.ComboBox();
             this.destination = new System.Windows.Forms.ComboBox();
             this.connectionlist = new System.Windows.Forms.ListView();
-            this.dep = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.des = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.deptime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.destime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.alldep = new System.Windows.Forms.Button();
+            this.alldep2 = new System.Windows.Forms.Button();
+            this.changedirection = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // searchconnection
             // 
             this.searchconnection.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.searchconnection.Location = new System.Drawing.Point(329, 51);
+            this.searchconnection.Location = new System.Drawing.Point(542, 107);
             this.searchconnection.Name = "searchconnection";
-            this.searchconnection.Size = new System.Drawing.Size(114, 35);
+            this.searchconnection.Size = new System.Drawing.Size(115, 35);
             this.searchconnection.TabIndex = 0;
             this.searchconnection.Text = "Verbindung Suchen";
             this.searchconnection.UseVisualStyleBackColor = true;
@@ -55,7 +55,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 36);
+            this.label1.Location = new System.Drawing.Point(76, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 3;
@@ -64,7 +64,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(34, 82);
+            this.label2.Location = new System.Drawing.Point(76, 82);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(36, 13);
             this.label2.TabIndex = 5;
@@ -73,7 +73,7 @@
             // departure
             // 
             this.departure.FormattingEnabled = true;
-            this.departure.Location = new System.Drawing.Point(129, 33);
+            this.departure.Location = new System.Drawing.Point(171, 33);
             this.departure.Name = "departure";
             this.departure.Size = new System.Drawing.Size(182, 21);
             this.departure.TabIndex = 6;
@@ -83,7 +83,7 @@
             // destination
             // 
             this.destination.FormattingEnabled = true;
-            this.destination.Location = new System.Drawing.Point(129, 79);
+            this.destination.Location = new System.Drawing.Point(171, 79);
             this.destination.Name = "destination";
             this.destination.Size = new System.Drawing.Size(182, 21);
             this.destination.TabIndex = 7;
@@ -92,54 +92,69 @@
             // 
             // connectionlist
             // 
-            this.connectionlist.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.dep,
-            this.des,
-            this.deptime,
-            this.destime});
-            this.connectionlist.Location = new System.Drawing.Point(46, 146);
+            this.connectionlist.Location = new System.Drawing.Point(0, 148);
             this.connectionlist.Name = "connectionlist";
-            this.connectionlist.Size = new System.Drawing.Size(545, 223);
+            this.connectionlist.Size = new System.Drawing.Size(657, 274);
             this.connectionlist.TabIndex = 8;
             this.connectionlist.UseCompatibleStateImageBehavior = false;
             this.connectionlist.View = System.Windows.Forms.View.Details;
             // 
-            // dep
-            // 
-            this.dep.Text = "Von";
-            this.dep.Width = 100;
-            // 
-            // des
-            // 
-            this.des.Text = "Nach";
-            this.des.Width = 100;
-            // 
-            // deptime
-            // 
-            this.deptime.Text = "Abfahrtszeit";
-            this.deptime.Width = 170;
-            // 
-            // destime
-            // 
-            this.destime.Text = "Ankunftszeit";
-            this.destime.Width = 170;
-            // 
             // alldep
             // 
             this.alldep.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.alldep.Location = new System.Drawing.Point(477, 51);
+            this.alldep.Location = new System.Drawing.Point(359, 25);
             this.alldep.Name = "alldep";
-            this.alldep.Size = new System.Drawing.Size(114, 35);
+            this.alldep.Size = new System.Drawing.Size(47, 35);
             this.alldep.TabIndex = 9;
-            this.alldep.Text = "Alle Verbindungen suchen ab";
+            this.alldep.Text = "Details";
             this.alldep.UseVisualStyleBackColor = true;
             this.alldep.Click += new System.EventHandler(this.alldep_Click);
+            // 
+            // alldep2
+            // 
+            this.alldep2.Location = new System.Drawing.Point(359, 71);
+            this.alldep2.Name = "alldep2";
+            this.alldep2.Size = new System.Drawing.Size(47, 35);
+            this.alldep2.TabIndex = 10;
+            this.alldep2.Text = "Details";
+            this.alldep2.UseVisualStyleBackColor = true;
+            this.alldep2.Click += new System.EventHandler(this.alldep2_Click);
+            // 
+            // changedirection
+            // 
+            this.changedirection.Location = new System.Drawing.Point(7, 46);
+            this.changedirection.Name = "changedirection";
+            this.changedirection.Size = new System.Drawing.Size(63, 38);
+            this.changedirection.TabIndex = 11;
+            this.changedirection.Text = "Richtung ändern";
+            this.changedirection.UseVisualStyleBackColor = true;
+            this.changedirection.Click += new System.EventHandler(this.changedirection_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(412, 25);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(184, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "HH:mm";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(602, 25);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(52, 20);
+            this.dateTimePicker2.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(642, 411);
+            this.ClientSize = new System.Drawing.Size(661, 427);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.changedirection);
+            this.Controls.Add(this.alldep2);
             this.Controls.Add(this.alldep);
             this.Controls.Add(this.connectionlist);
             this.Controls.Add(this.destination);
@@ -162,11 +177,11 @@
         private System.Windows.Forms.ComboBox departure;
         private System.Windows.Forms.ComboBox destination;
         private System.Windows.Forms.ListView connectionlist;
-        private System.Windows.Forms.ColumnHeader dep;
-        private System.Windows.Forms.ColumnHeader des;
-        private System.Windows.Forms.ColumnHeader deptime;
-        private System.Windows.Forms.ColumnHeader destime;
         private System.Windows.Forms.Button alldep;
+        private System.Windows.Forms.Button alldep2;
+        private System.Windows.Forms.Button changedirection;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
     }
 }
 
--- Transport-Abfragen/Form1.cs
+++ Transport-Abfragen/Form1.cs
@@ -18,30 +18,45 @@ namespace Transport_Abfragen
             InitializeComponent();
         }
 
+        //Verbindung zwischen den beiden Stationen suchen
         private void searchconnection_Click_1(object sender, EventArgs e)
         {
+            connectionlist.Clear();
+
+            connectionlist.Columns.Add("From", 100, HorizontalAlignment.Left);
+            connectionlist.Columns.Add("To", 100, HorizontalAlignment.Left);
+            connectionlist.Columns.Add("Plattform", 60, HorizontalAlignment.Left);
+            connectionlist.Columns.Add("Departure", 140, HorizontalAlignment.Left);
+            connectionlist.Columns.Add("Arrival", 140, HorizontalAlignment.Left);
+            connectionlist.Columns.Add("Duration", 100, HorizontalAlignment.Left);
+
             Transport connection = new Transport();
             Connections connections = new Connections();
             
-
+            //Verbindung holen
             connections = connection.GetConnections(departure.Text, destination.Text);
 
 
 
             foreach (var con in connections.ConnectionList)
             {
+                //Zeitformat anpassen
                 DateTime convertedDateDep = DateTime.Parse(con.From.Departure);
                 DateTime convertedDateArr = DateTime.Parse(con.To.Arrival);
 
+                //Liste mit Verbindungen erstellen und Verbindungen adden
                 ListViewItem lvlist = new ListViewItem(con.From.Station.Name);
                 lvlist.SubItems.Add(con.To.Station.Name);
+                lvlist.SubItems.Add(con.From.Platform);
                 lvlist.SubItems.Add(convertedDateDep.ToString());
                 lvlist.SubItems.Add(convertedDateArr.ToString());
+                lvlist.SubItems.Add(con.Duration);
                 connectionlist.Items.Add(lvlist);
             }
             
         }
 
+        //Stations vorschläge holen und in Dropdown adden
         private void departure_TextUpdate(object sender, EventArgs e)
         {
             departure.Items.Clear();
@@ -52,14 +67,17 @@ namespace Transport_Abfragen
             Stations to = new Stations();
             Transport transport = new Transport();
 
+            //Stationen suchen
             from = transport.GetStations(departure.Text);
 
+            //Stationen in droppdown adden
             foreach (Station hold in from.StationList)
             {
                 departure.Items.Add(hold.Name);
             }
         }
 
+        //Stations vorschläge holen und in Dropdown adden
         private void destination_TextUpdate(object sender, EventArgs e)
         {
             destination.Items.Clear();
@@ -69,44 +87,129 @@ namespace Transport_Abfragen
             Stations from = new Stations();
             Stations to = new Stations();
             Transport transport = new Transport();
-
+            
+            //Stationen suchen
             from = transport.GetStations(destination.Text);
 
+            //Stationen in droppdown adden
             foreach (Station hold in from.StationList)
             {
                 destination.Items.Add(hold.Name);
             }
         }
 
+        //Dropdown auf dem Felder aktivieren beim Reinklicken
         private void destination_MouseClick(object sender, MouseEventArgs e)
         {
             destination.DroppedDown = true;
         }
 
+        //Dropdown auf dem Felder aktivieren beim Reinklicken
         private void departure_MouseClick(object sender, MouseEventArgs e)
         {
             departure.DroppedDown = true;
         }
 
+        //Station board laden und anzeigen für Abfahrtsort
         private void alldep_Click(object sender, EventArgs e)
         {
+            connectionlist.Clear();
+
+            //Spalten in der Liste erstellen
+            connectionlist.Columns.Add("", 50, HorizontalAlignment.Left);
+            connectionlist.Columns.Add("Name", 60, HorizontalAlignment.Left);
+            connectionlist.Columns.Add("To", 130, HorizontalAlignment.Left);
+            connectionlist.Columns.Add("Departure", 130, HorizontalAlignment.Left);
+
             Transport connection = new Transport();
             Connections connections = new Connections();
 
-            string id = connection.GetStations(departure.Text).StationList.First().Id;
-            // 1. Alle Stationen werden geladen, Parameter wird Text von Combobox mitgegeben.
-            // 2. Von der Rückgabe Liste, wird das erste Element ausgewählt.
-            // 3. Von diesem Element wird die ID ausgeleden.
-
-            StationBoardRoot stationboard = connection.GetStationBoard(departure.Text, id);
-
-            foreach (var board in stationboard.ToString())
+            //Auf leeres Feld prüfen
+            if (departure.Text != "")
             {
-                //Korriegieren
-                ListViewItem lvlist = new ListViewItem(stationboard.ToString());
-                connectionlist.Items.Add(lvlist);
+                                string id = connection.GetStations(departure.Text).StationList.First().Id;
+                // 1. Alle Stationen werden geladen, Parameter wird Text von Combobox mitgegeben.
+                // 2. Von der Rückgabe Liste, wird das erste Element ausgewählt.
+                // 3. Von diesem Element wird die ID ausgeleden.
+
+                //Mithilfe vom Text in der Combobox und der ID werden alle Verbindungen geholt. 
+                StationBoardRoot stationboard = connection.GetStationBoard(departure.Text, id);
+
+                int temp = 1;
+
+                foreach (var board in stationboard.Entries)
+                {
+                    //Jedes Item wird der Liste hinzugefügt. 
+                    ListViewItem lvlist = new ListViewItem(temp.ToString());
+                    lvlist.SubItems.Add(board.Name);
+                    lvlist.SubItems.Add(board.To);
+                    lvlist.SubItems.Add(board.Stop.Departure.ToString());
+                    connectionlist.Items.Add(lvlist);
+
+                    temp++;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Das Feld darf nicht leer sein!");
             }
             
+        }
+
+        //Station board laden und anzeigen für Ankunftsort
+        private void alldep2_Click(object sender, EventArgs e)
+        {
+            connectionlist.Clear();
+
+            //Spalten in der Liste erstellen
+            connectionlist.Columns.Add("", 50, HorizontalAlignment.Left);
+            connectionlist.Columns.Add("Name", 60, HorizontalAlignment.Left);
+            connectionlist.Columns.Add("To", 130, HorizontalAlignment.Left);
+            connectionlist.Columns.Add("Departure", 130, HorizontalAlignment.Left);
+
+            Transport connection = new Transport();
+            Connections connections = new Connections();
+
+            //Auf leeres Feld prüfen
+            if (destination.Text != "")
+            {      
+                string id = connection.GetStations(destination.Text).StationList.First().Id;
+                // 1. Alle Stationen werden geladen, Parameter wird Text von Combobox mitgegeben.
+                // 2. Von der Rückgabe Liste, wird das erste Element ausgewählt.
+                // 3. Von diesem Element wird die ID ausgeleden.
+
+                //Mithilfe vom Text in der Combobox und der ID werden alle Verbindungen geholt. 
+                StationBoardRoot stationboard = connection.GetStationBoard(departure.Text, id);
+
+                int temp = 1;
+
+                foreach (var board in stationboard.Entries)
+                {
+                    //Jedes Item wird der Liste hinzugefügt.
+                    ListViewItem lvlist = new ListViewItem(temp.ToString());
+                    lvlist.SubItems.Add(board.Name);
+                    lvlist.SubItems.Add(board.To);
+                    lvlist.SubItems.Add(board.Stop.Departure.ToString());
+                    connectionlist.Items.Add(lvlist);
+
+                temp++;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Das Feld darf nicht leer sein!");
+            }
+        }
+
+        //Abfahrts- und Ankunftsort vertauschen
+        private void changedirection_Click(object sender, EventArgs e)
+        {
+            string temp = departure.Text;
+            departure.Text = destination.Text;
+            destination.Text = temp;
+
+            destination.Items.Clear();
+            departure.Items.Clear();
         }
     }
 }
